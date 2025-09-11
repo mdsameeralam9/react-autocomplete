2132e892e2ff7d569a14800c9c7a8ee2a5aef577
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-09-11 19:18:26 +0530

    setup seacrh input component

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 
 const AutoComplete = () => {
   const [search, setSearch] = useState('');
-  
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
 
   return (
     <div className="m-4">
       <div className="inputWrap ">
-        <input type="text" placeholder="Search" className="border w-1/2" />
+        <input value={search} type="text" placeholder="Search" className="border w-1/2" onChange={handleChange}/>
       </div>
 
       <div className="dataCompone border p-2 w-1/2 scroll-auto">
